01508a833eecd79497e4c859ff5cc5c0610ee858
Ravindu Suriarachchi <Ravindu Suriarachchi>
2016-10-22 10:54:35 +0530

    fix contoller issue

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,8 +16,7 @@ angular.module('starter.controllers', ['firebase'])
 
 .controller('ChatsCtrl', ['$scope', '$firebaseArray', '$state', '$stateParams', '$rootScope',
         function ($scope, $firebaseArray, $rootScope, $state) {
-            screen.unlockOrientation();
-
+           screen.unlockOrientation();
             var ref = new Firebase('https://kiddo-56f35.firebaseio.com/chat');
             var sync = $firebaseArray(ref);
             $scope.chats = sync;
@@ -59,7 +58,7 @@ angular.module('starter.controllers', ['firebase'])
             catch (err) {
                 var arr = [];
                 arr.push(vreply);
-                chats.push("reply", arr);
+                chats.reply = arr;
             }
             $scope.chats.$save(chat).then(function (sync) {
                 sync.key() === chat.$id; // true
@@ -153,3 +152,94 @@ angular.module('starter.controllers', ['firebase'])
             });
 
         })
+
+        .controller('LoginCtrl', function ($scope, $ionicPopup, $state, Child,User) {
+
+                  $scope.user=User;
+                  $scope.child = Child;
+
+          $scope.login = function(data) {
+
+            var email = data.username;
+            var pass = data.password;
+            console.log("Email:", email);
+            console.log("pass:", pass);
+
+            var user =firebase.auth().signInWithEmailAndPassword(email, pass).then(function(user){
+
+                console.log('logged in:', user);
+                $state.go('tab.dash');
+                console.log(user.created);
+
+            }).catch(function(error) {
+                            // Handle Errors here.
+                      var errorCode = error.code;
+                      var errorMessage = error.message;
+                      var alertPopup = $ionicPopup.alert({
+                          title: 'Invalid Login',
+                          template: errorMessage
+                      });//alert
+
+            });//catch
+
+          }//$scope.login
+
+          $scope.register = function () {
+
+            $state.go('register');
+
+          }//$scope.register
+
+          $scope.addChildAccount = function (data) {
+
+            $scope.child.$add({
+              username: data.username,
+              name:data.name ,
+              grade:data.grade,
+              password:data.password
+            });
+
+            $scope.user.$add({
+              username: data.username,
+              password: data.password,
+              type:"child"
+            });
+
+            console.log(data);
+            var alertPopup = $ionicPopup.alert({
+                title: 'Success',
+                template: 'Child Account Successfully Created!'
+            });
+            $state.go('tab.dash');
+          };//$scope.addChildAccount
+
+          $scope.addUserEmail = function (data) {
+
+              var email = data.email;
+              var pass = data.password;
+
+                console.log("Email:", email);
+                console.log("pass:", pass);
+
+              firebase.auth().createUserWithEmailAndPassword(email, pass).then(function(user){
+                  console.log('logged in:', user);
+                  login(data);
+                })
+                .catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Invalid SignUp',
+                    template: errorCode
+                });//alert
+
+            });
+
+          };//$scope.addUserEmail
+
+          $scope.userLogged = function()
+          {
+            console.log("hjhjhjk--------------------");
+          }
+        });
